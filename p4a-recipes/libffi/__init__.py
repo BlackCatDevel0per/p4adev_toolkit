b02ca804c734132b66c7bb564c940c1b01bb4104
Asad Sayfiyev <bcdev@mail.ru>
2024-04-10 06:23:21 +0630

    Update __init__.py

--- p4a-recipes/libffi/__init__.py
+++ p4a-recipes/libffi/__init__.py
@@ -1,41 +1,48 @@
-from os.path import exists, join
 from multiprocessing import cpu_count
-from pythonforandroid.recipe import Recipe
-from pythonforandroid.logger import shprint
-from pythonforandroid.util import current_directory
+from os.path import join
+
 import sh
+from pythonforandroid.logger import shprint
+from pythonforandroid.recipe import Recipe
+from pythonforandroid.util import current_directory
 
 
 class LibffiRecipe(Recipe):
-    """
-    Requires additional system dependencies on Ubuntu:
-        - `automake` for the `aclocal` binary
-        - `autoconf` for the `autoreconf` binary
-        - `libltdl-dev` which defines the `LT_SYS_SYMBOL_USCORE` macro
-    """
-    name = 'libffi'
-    version = 'v3.4.6'
-    url = 'https://github.com/libffi/libffi/archive/{version}.tar.gz'
+	"""libffi recipe.
 
-    patches = ['remove-version-info.patch']
+	Require additional system dependencies on Ubuntu:
+		- `automake` for the `aclocal` binary
+		- `autoconf` for the `autoreconf` binary
+		- `libltdl-dev` which defines the `LT_SYS_SYMBOL_USCORE` macro
+	"""
 
-    built_libraries = {'libffi.so': '.libs'}
+	name = 'libffi'
+	version = 'v3.4.6'
+	url = 'https://github.com/libffi/libffi/archive/{version}.tar.gz'
 
-    def build_arch(self, arch):
-        env = self.get_recipe_env(arch)
-        with current_directory(self.get_build_dir(arch.arch)):
-            # if not exists('configure'):
-            #     shprint(sh.Command('./autogen.sh'), _env=env)
-            shprint(sh.Command('autoreconf'), '-vif', _env=env)
-            shprint(sh.Command('./configure'),
-                    '--host=' + arch.command_prefix,
-                    '--prefix=' + self.get_build_dir(arch.arch),
-                    '--disable-builddir',
-                    '--enable-shared', _env=env)
-            shprint(sh.make, '-j', str(cpu_count()), 'libffi.la', _env=env)
+	patches = ['remove-version-info.patch']
 
-    def get_include_dirs(self, arch):
-        return [join(self.get_build_dir(arch.arch), 'include')]
+	built_libraries = {'libffi.so': '.libs'}
+
+	def build_arch(self, arch):
+		env = self.get_recipe_env(arch)
+		with current_directory(self.get_build_dir(arch.arch)):
+			# old way:
+			# if not exists('configure'):
+			#     shprint(sh.Command('./autogen.sh'), _env=env)
+			shprint(sh.Command('autoreconf'), '-vif', _env=env)
+			shprint(
+				sh.Command('./configure'),
+				'--host=' + arch.command_prefix,
+				'--prefix=' + self.get_build_dir(arch.arch),
+				'--disable-builddir',
+				'--enable-shared',
+				_env=env,
+			)
+			shprint(sh.make, '-j', str(cpu_count()), 'libffi.la', _env=env)
+
+	def get_include_dirs(self, arch):
+		return [join(self.get_build_dir(arch.arch), 'include')]
 
 
 recipe = LibffiRecipe()
