f5bd0f93863f4db7df7b70ae6fd8f3f387ee0430
Asad Sayfiyev <bcdev@mail.ru>
2024-05-16 08:35:46 +0630

    Add selection rw checkboxes

--- src/app/Controller/main_screen.py
+++ src/app/Controller/main_screen.py
@@ -2,11 +2,18 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from kivymd.uix.textfield import MDTextField
+
 from app.Controller.base_controller import BaseController
 from app.View.main_screen import MainScreenView
 
 if TYPE_CHECKING:
 	from typing import Any
+
+	from kivymd.uix.anchorlayout import MDAnchorLayout
+	from kivymd.uix.selectioncontrol import MDCheckbox
+
+	from app.Model.main_screen import MainScreenModel
 
 
 class MainScreenController(BaseController):
@@ -17,7 +24,50 @@ class MainScreenController(BaseController):
 	the view to control its actions.
 	"""
 
+	view: 'MainScreenView'
+	model: 'MainScreenModel'
+
+
 	def __init__(self: 'MainScreenController', *args: 'Any', **kwargs: 'Any') -> None:
 		super().__init__(*args, **kwargs)
-
+		# TODO: Set view automatically too..
 		self._view = MainScreenView(controller=self, model=self.model)
+
+
+	def __post_init__(self: 'MainScreenController') -> None:
+		super().__post_init__()
+
+		# get object by usually ref
+		self.model.write_txtfield = MDTextField()
+
+		read_checkbox: 'MDCheckbox' = self.view.ids.rw_checkboxes.read_checkbox.ids.selection_checkbox
+		self.selection_on_read_active(read_checkbox, read_checkbox.active)
+
+
+	def selection_on_read_active(
+		self: 'MainScreenController',
+		checkbox: 'MDCheckbox', value: bool,
+	) -> None:
+		# TODO: Add attrs to model
+		layout: 'MDAnchorLayout' = self.view.ids.layout_write_txtfield
+
+		if checkbox.group != 'select_rw':
+			self.log.warning(
+				'Widget SM: Callback was called by incorrect widget `%s`, must be `SelectMode`.read_checkbox',
+				checkbox.group,
+			)
+
+		if value:
+			# Clear layout widgets
+			layout.clear_widgets()
+		else:
+			# Back add widget to layout
+			if layout.children:
+				self.log.warning(
+					'Widget SM: Widget `%s` has children: `%s` '
+					'(this mean checkbox was activated twice for some reasons..)',
+					layout,
+					layout.children,
+				)
+				return
+			layout.add_widget(self.model.write_txtfield)
--- src/app/Model/main_screen.py
+++ src/app/Model/main_screen.py
@@ -1,12 +1,25 @@
 from __future__ import annotations
 
-from kivy.metrics import dp
-from plyer.utils import platform
+from typing import TYPE_CHECKING
 
+# from kivy.metrics import dp
+# from plyer.utils import platform
 # if platform == 'android':
 # 	from android import mActivity
 from .base_model import BaseScreenModel
 
+if TYPE_CHECKING:
+	from kivy.uix.textfield import MDTextField
+
 
 class MainScreenModel(BaseScreenModel):
 	"""Implements the logic of the MainScreenView class."""
+
+	# TODO: Make special metaclass for this stuff..
+	@property
+	def write_txtfield(self: 'MainScreenModel') -> 'MDTextField':
+		return self._write_txtfield
+
+	@write_txtfield.setter
+	def write_txtfield(self: 'MainScreenModel', value: 'MDTextField') -> None:
+		self._write_txtfield = value
