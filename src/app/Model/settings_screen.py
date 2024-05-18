f18f3829c9cd2c4c19b6a3b5ff81dab3c301a793
Asad Sayfiyev <bcdev@mail.ru>
2024-05-18 09:12:40 +0630

    [Settings Screen] Move some stuff into the model

--- src/app/Model/settings_screen.py
+++ src/app/Model/settings_screen.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
-from kivy.metrics import dp
+from kivymd.toast import toast
 from plyer.utils import platform
+from PyScopedStorage.filechooser import filechooser
+
+from app.utility.utils import StrCall
 
 # if platform == 'android':
 # 	from android import mActivity
@@ -10,3 +13,49 @@ from .base_model import BaseScreenModel
 
 class MainScreenModel(BaseScreenModel):
 	"""Implements the logic of the MainScreenView class."""
+
+
+	# TODO: Better use setter-getter & write.. && mb fully wrap usually conf)
+	@property
+	def docs_path_conf(self: 'MainScreenModel') -> str:
+		return self.config.get('app', 'docs_path')
+
+
+	@docs_path_conf.setter
+	def docs_path_conf(self: 'MainScreenModel', uri: str) -> None:
+		self.config.set('app', 'docs_path', uri)
+		self.config.write()
+
+		toast('Docs path set!')
+
+
+	def set_docs_path_callback(self: 'MainScreenModel', sel: 'list[android.net.Uri]') -> None:
+		if not sel:
+			return
+
+		uri_or_path: 'android.net.Uri | str' = sel[0]
+
+		if platform != 'android':
+			self.log.debug('Access path: `%s`', uri_or_path)
+			assert isinstance(uri_or_path, str)  # linter plug
+			self.docs_path_conf = uri_or_path  # TODO: Solve normally -_-
+			self.notify_observers('settings_screen')
+			return
+
+		self.log.debug('Access SS Uri: %s', StrCall(uri_or_path.toString))
+
+		uri_full_path: str = uri_or_path.toString()
+
+		self.docs_path_conf = uri_full_path
+		self.notify_observers('settings_screen')
+
+
+	def set_docs_path(self: 'MainScreenModel') -> None:
+		filechooser.choose_dir(
+			# on_selection=lambda s: self.log.warning(f'result: {s}'),
+			on_selection=self.set_docs_path_callback,
+		)
+
+
+	def save_settings(self: 'MainScreenModel', btn) -> None:
+		...
--- src/app/View/settings_screen/platforms/mobile/main_screen.py
+++ src/app/View/settings_screen/platforms/mobile/main_screen.py
@@ -2,17 +2,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from app.utility.utils import StrCall
 from app.View.base_screen import BaseScreenView
-from kivymd.toast import toast
-from plyer.utils import platform
-from PyScopedStorage.filechooser import filechooser
 
 if TYPE_CHECKING:
-	from typing import Callable
+	...
 
 
-# TODO: Full screen mode & hide status bar & more comfartable read functions..
+# TODO: Full screen mode & hide status bar..
 
 
 class SettingsScreenView(BaseScreenView):
@@ -21,6 +17,7 @@ class SettingsScreenView(BaseScreenView):
 		...
 
 
+	# FIXME: Move into controller
 	def _validate_app_client_id(self: 'SettingsScreenView', text: str) -> bool:
 		return not text.isnumeric()
 
@@ -31,51 +28,15 @@ class SettingsScreenView(BaseScreenView):
 		return not bool(text)
 
 
-	def set_docs_path_conf(self: 'SettingsScreenView', uri: str) -> None:
-		self.app.config.set('app', 'docs_path', uri)
-
-		self.app.config.write()
-
-		toast('Docs path set!')
-
-
-	def set_scdir_callback(self: 'SettingsScreenView', sel: 'list[android.net.Uri]') -> None:
-		if not sel:
-			return
-
-		uri_or_path: 'android.net.Uri | str' = sel[0]
-
-		if platform != 'android':
-			self.log.debug('Access path: `%s`', uri_or_path)
-			assert isinstance(uri_or_path, str)  # linter plug
-			self.set_docs_path_conf(uri_or_path)  # TODO: Solve normally -_-
-			return
-
-		self.log.debug('Access SS Uri: %s', StrCall(uri_or_path.toString))
-
-		dir_full_path: str = uri_or_path.toString()
-
-		self.set_docs_path_conf(dir_full_path)
-
-
-	def set_scdir(self: 'SettingsScreenView') -> None:
-		filechooser.choose_dir(
-			# on_selection=lambda s: self.log.warning(f'result: {s}'),
-			on_selection=self.set_scdir_callback,
-		)
-
-
-	# TODO: Move into the model
-	def save_settings(self: 'SettingsScreenView', btn) -> None:
-		...
-
-
 	def model_is_changed(self: 'SettingsScreenView') -> None:
 		"""Call whenever any change has occurred in the data model.
 
 		The view in this method tracks these changes and updates the UI
 		according to these changes.
 		"""
+		# Update label of `set_docs` action
+		self.ids.main_bl_startup_tools_set_docs_path_tool.\
+			label_text = self.app.config.get('app', 'docs_path')
 
 
 MainScreenView = SettingsScreenView
